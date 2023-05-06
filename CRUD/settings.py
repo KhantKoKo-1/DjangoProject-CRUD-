f2d61b291b9cf57f5106c6d1fdcca1caa32b32da
re mo <khantkoko5352@gmail.com>
2023-05-06 19:34:44 +0630

    model

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -123,12 +123,8 @@ MEDIA_URL = '/media/'
 
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[BASE_DIR / 'static']
 
-
-
-STATICFTLES_DIRS =(
-    os.path.join(BASE_DIR,'static')
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
